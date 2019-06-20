bf6ad35190be2863678911d5d843d8e80614efa0
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-20 22:42:56 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,18 +1,17 @@
 def dictionary
-words-to-be-substituted = {
-  "hello" => "hi",
-  "to" => "2",
-  "two" => "2",
-  "too" => "2",
-  "four" => "4",
-  "for" => "4",
-  "be" => "b",
-  "you" => "u",
-  "at" => "@",
-  "and" => "&"
-}
+    dictionary_hash = {
+        "hello" => "hi",
+        "to" => "2",
+        "two" => "2",
+        "too" => "2",
+        "for" => "4",
+        "four" => "4",
+        "be" => "b",
+        "you" => "u",
+        "at" => "@",
+        "and" => "&"
+        }
 end
-
 def word_substituter(tweet)
     tweet_array = tweet.split(" ")
     tweet_array.map do |word|
